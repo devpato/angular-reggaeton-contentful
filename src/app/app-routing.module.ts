aade27a36371a283daca96518a3798cce6b78b34
devpato <pevargas2730@gmail.com>
2020-04-21 01:39:16 -0400

    base code done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PostsPageComponent } from './posts-page/posts-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PostsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
