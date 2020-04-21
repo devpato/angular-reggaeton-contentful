895050ac06a9aeb778948116174ff8cf9e6ae5fd
devpato <pevargas2730@gmail.com>
2020-04-20 22:22:31 -0400

    posts page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsPageComponent } from './posts-page/posts-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsPageComponent
   ],
   imports: [
     BrowserModule,
